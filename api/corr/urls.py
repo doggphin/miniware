abb4fa34120342eab2209614d13ac604b02b1ba2
Doggphin <doggphin@gmail.com>
2025-03-15 16:09:32 -0600

    adding correct vhs endpoint

--- api/corr/urls.py
+++ api/corr/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('slides/<str:from_folder>/<str:to_folder>/', views.correct_slides),
     path('prints/<str:from_folder>/<str:to_folder>/', views.correct_prints),
     path('audio/<str:from_folder>/<str:to_folder>/', views.correct_audio),
+    path('vhs/<str:from_folder>/<str:to_folder>/', views.correct_vhs)
     path('all/<str:project_folder>/', views.correct_all)
 ]
